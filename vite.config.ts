ea3179c3eee3b761efd7783200247da714023abc
闻人放歌 <wenrenfangge@gmail.com>
2024-10-23 11:28:06 +0800

    fix: install包无法解析Cesium type文件问题解决

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 闻人放歌 wenrenfangge@gmail.com
  * @Date: 2024-10-20 10:46:51
  * @LastEditors: 闻人放歌 wenrenfangge@gmail.com
- * @LastEditTime: 2024-10-22 11:51:24
+ * @LastEditTime: 2024-10-23 11:26:31
  * @FilePath: /cesiumDraw/Users/wenrenfangge/Documents/wenrenfangge-studio/frontEnd/cesium-draw/vite.config.ts
  * @Description: vite配置
  */
